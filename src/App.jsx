a9e3fe950397b4053390cc0b2505a111dc5f220e
Asep Angga Ihza S <126168431+anggaihza@users.noreply.github.com>
2024-11-16 21:58:30 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
